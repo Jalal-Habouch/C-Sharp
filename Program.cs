d00924824bf47b5425b96b35b53800704d52d3c0
Jalal <Jalal-Habouch@users.noreply.github.com>
2021-11-15 23:54:47 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jalal, Do you want to learn C#?");
+        }
+    }
+}
